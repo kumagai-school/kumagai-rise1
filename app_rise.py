4d1a9c3732434cc3cdaa6a28b7bba7924dc21aa2
kumagai-school <yoshii@kumagai-c.com>
2025-06-24 13:57:37 +0900

    Add files via upload

--- app_rise.py
+++ app_rise.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import requests
 
-st.set_page_config(page_title="上昇銘柄リスト", layout="wide")
-st.title("📈 上昇銘柄 抽出リスト")
+st.set_page_config(page_title="スクリーニング", layout="wide")
+st.title("📈 [ルール1]スクリーニング")
 
 # データ取得関数
 def load_data(source):
@@ -16,8 +16,8 @@ def load_data(source):
         return pd.DataFrame()
 
 # ラジオボタン選択
-option = st.radio("表示対象を選んでください", ["本日の抽出", "昨日の抽出"], horizontal=True)
-data_source = "today" if option == "本日の抽出" else "yesterday"
+option = st.radio("表示対象を選んでください", ["本日の抽出結果", "昨日の抽出結果"], horizontal=True)
+data_source = "today" if option == "本日の抽出結果" else "yesterday"
 df = load_data(data_source)
 
 # 表示処理
@@ -26,8 +26,8 @@ if df.empty:
 else:
     try:
         # カラム整形
-        df = df[["code", "low", "low_date", "high", "high_date", "倍率"]]
-        df.columns = ["銘柄コード", "最安値", "最安値日", "高値", "高値日", "倍率"]
+        df = df[["code", "name", "low", "low_date", "high", "high_date", "倍率"]]
+        df.columns = ["銘柄コード", "企業名", "安値", "安値日", "高値", "高値日", "倍率"]
         df["倍率"] = pd.to_numeric(df["倍率"], errors="coerce").map(lambda x: f"{x:.2f}倍")
 
         # 銘柄コード列にHTMLリンクを埋め込み
@@ -40,3 +40,30 @@ else:
 
     except Exception as e:
         st.error(f"データ整形中のエラー: {e}")
+st.markdown("---")
+st.markdown("<h4>📌 <strong>注意事項</strong></h4>", unsafe_allow_html=True)
+
+st.markdown("""
+<div style='color:red; font-size:14px;'>
+<ul>
+  <li>抽出結果にはETFなどルール1対象でないものも表示されます。</li>
+  <li>平日8時30分～9時に5分程度のメンテナンスが入ることがあります。</li>
+  <li>「本日の抽出結果」はおおよそ1時間ごとの更新となります。</li>
+</ul>
+</div>
+""", unsafe_allow_html=True)
+
+st.markdown("---")
+st.markdown("""
+<div style='
+    text-align: center;
+    color: gray;
+    font-size: 14px;
+    font-family: "Segoe UI", "Helvetica Neue", "Arial", sans-serif !important;
+    letter-spacing: 0.5px;
+    unicode-bidi: plaintext;
+'>
+&copy; 2025 KumagaiNext All rights reserved.
+</div>
+""", unsafe_allow_html=True)
+
