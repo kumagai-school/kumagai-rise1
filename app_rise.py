bf9aa907341d272ec61f7c6513b3534428af9c52
kumagai-school <yoshii@kumagai-c.com>
2025-06-24 15:35:48 +0900

    Add files via upload

--- app_rise.py
+++ app_rise.py
@@ -14,7 +14,7 @@ st.markdown("<h4>📌 <strong>注意事項</strong></h4>", unsafe_allow_html=Tru
 st.markdown("""
 <div style='color:red; font-size:13px;'>
 <ul>
-  <li>抽出結果にはＥＴＦなどルール1対象でないものも表示されます</li>
+  <li>抽出結果にはETFなどルール1対象でないものも表示されます</li>
   <li>平日8時30分～9時に10分程度のメンテナンスが入ることがあります</li>
   <li>「本日の抽出結果」はおおよそ1時間ごとの更新となります</li>
 </ul>
@@ -63,12 +63,13 @@ st.markdown("<h4>📌 <strong>注意事項</strong></h4>", unsafe_allow_html=Tru
 st.markdown("""
 <div style='color:red; font-size:13px;'>
 <ul>
-  <li>抽出結果にはＥＴＦなどルール1対象でないものも表示されます</li>
+  <li>抽出結果にはETFなどルール1対象でないものも表示されます</li>
   <li>平日8時30分～9時に10分程度のメンテナンスが入ることがあります</li>
   <li>「本日の抽出結果」はおおよそ1時間ごとの更新となります</li>
 </ul>
 </div>
 """, unsafe_allow_html=True)
+
 
 st.markdown("---")
 st.markdown("""
